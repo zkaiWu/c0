0e496cde5760aa1c6856b88ede72d61fbb701655
wzk1998 <869693441@qq.com>
2020-12-06 09:33:21 +0800

    first submit test

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+
+if test $1 = '-c'
+then
+  gradle fatjar --no-daemon
+fi
+#echo "input file is"
+#read input
+#echo "output file is"
+#read output
+java -jar ./build/libs/c0.jar input.txt -o output.txt
---
+++ src/main/java/c0/App.java
@@ -0,0 +1,76 @@
+package c0;
+
+
+import net.sourceforge.argparse4j.*;
+import net.sourceforge.argparse4j.impl.Arguments;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
+import net.sourceforge.argparse4j.inf.Namespace;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class App<scanner> {
+    public static void main(String[] args) {
+        var argparse = buildArgparse();
+        Namespace result;
+        try {
+            result = argparse.parseArgs(args);
+        } catch (ArgumentParserException e1) {
+            argparse.handleError(e1);
+            return;
+        }
+
+        var inputFileName = result.getString("input");
+        var outputFileName = result.getString("output");
+
+        InputStream input;
+        if (inputFileName.equals("-")) {
+            input = System.in;
+        } else {
+            try {
+                input = new FileInputStream(inputFileName);
+            } catch (FileNotFoundException e) {
+                System.err.println("Cannot find input file.");
+                e.printStackTrace();
+                System.exit(2);
+                return;
+            }
+        }
+
+        PrintStream output;
+        if (outputFileName.equals("-")) {
+            output = System.out;
+        } else {
+            try {
+                output = new PrintStream(new FileOutputStream(outputFileName));
+            } catch (FileNotFoundException e) {
+                System.err.println("Cannot open output file.");
+                e.printStackTrace();
+                System.exit(2);
+                return;
+            }
+        }
+
+        Scanner scanner = new Scanner(input);
+        output.println("haha");
+
+    }
+
+
+    private static ArgumentParser buildArgparse() {
+        var builder = ArgumentParsers.newFor("miniplc0-java");
+        var parser = builder.build();
+        parser.addArgument("-o", "--output").help("Set the output file").required(true).dest("output")
+                .action(Arguments.store());
+        parser.addArgument("file").required(true).dest("input").action(Arguments.store()).help("Input file");
+        return parser;
+    }
+}
